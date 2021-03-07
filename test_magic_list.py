9d1a9de3407bfd1dc6d2a620429440b797daee1a
Yoav Caspi <yoavcaspi@gmail.com>
2021-03-07 16:16:32 +0200

    add more advanced tests.

--- test_magic_list.py
+++ test_magic_list.py
@@ -12,6 +12,26 @@ def test_skip_boundary_check_with_0():
     assert a[0] == 5
 
 
+def test_skip_boundary_check_with_valid_boundary_advanced():
+    # Given a magic list
+    # When accessing it with "valid"  boundary check
+    # We wish to add the value to the list.
+    a = MagicList()
+    for i in range(10):
+        assert a == list(range(i))
+        a[i] = i
+
+
+def test_skip_boundary_check_with_valid_negative_boundary_advanced():
+    # Given a magic list
+    # When accessing it with "valid"  boundary check
+    # We wish to add the value to the list.
+    a = MagicList()
+    for i in range(10):
+        assert a == list(range(i))
+        a[-1] = i
+
+
 def test_skip_boundary_check_with_minus_one():
     # Given a magic list
     # When accessing it with "valid"  boundary check
