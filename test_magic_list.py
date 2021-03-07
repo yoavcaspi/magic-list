3b751e852a876c71f477f8f743a95477813603b1
Yoav Caspi <yoavcaspi@gmail.com>
2021-03-07 16:08:33 +0200

    more tests for invalid boundaries.

--- test_magic_list.py
+++ test_magic_list.py
@@ -12,5 +12,26 @@ def test_skip_boundary_check_with_0():
     assert a[0] == 5
 
 
+@pytest.mark.parametrize("val", range(2, 10))
+def test_skip_boundary_check_with_bigger_gap(val):
+    # Given a magic list
+    # When accessing it with "invalid"  boundary check
+    # We wish to add the value to the list.
+    a = MagicList()
+
+    with pytest.raises(IndexError):
+        a[val] = 5
+
+
+@pytest.mark.parametrize("val", range(2, 10))
+def test_skip_boundary_check_with_negative_indexes(val: int):
+    # Given a magic list
+    # When accessing it with "invalid" boundary check
+    # We wish to add the value to the list.
+    a = MagicList()
+    with pytest.raises(IndexError):
+        a[-val] = 5
+
+
 if __name__ == '__main__':
     pytest.main()
